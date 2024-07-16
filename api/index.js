34f23b92b0f221aa118c6deb83f735b31b2ab685
teedev78 <i7891011t@gmail.com>
2024-07-16 14:04:53 +0700

    fix cors access

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const authRoute = require("./routes/auth");
@@ -14,36 +14,9 @@ mongoose
   .then(() => console.log("Connection Complete"))
   .catch((err) => console.log(err));
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-
-module.exports = allowCors(handler);
-
 //middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 //routes
 app.get("/", (req, res) => {
